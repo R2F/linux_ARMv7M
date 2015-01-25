25bbbe177eea7ef0692b76b3e0c21c73433684dc
Kamil Lulko <rev13@wp.pl>
2015-01-25 13:46:54 +0100

    Use openocd from PATH

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 # stop remaining openocd instances
 killall -9 openocd
 # start new openocd and reset and init chip
-/home/limak/bin/openocd -f board/stm32f429discovery.cfg -c init -c"reset init" -c"poll"
+openocd -f board/stm32f429discovery.cfg -c init -c"reset init" -c"poll"
